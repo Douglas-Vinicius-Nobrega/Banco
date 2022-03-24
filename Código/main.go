ae6c7621ba5b08f79428a512c7188e7580f25ed5
Douglas Vinicius Nobrega <dnobregac2@gmail.com>
2022-03-24 10:16:40 -0300

    Estudos dia 24-03

---
+++ Código/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	titular := "Douglas"
+	numeroAgencia := 589
+	numeroConta := 123456
+	saldo := 125.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+}
